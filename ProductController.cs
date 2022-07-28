c05653cd3965786a49d9f92313a4f1765fe8002c
Sallal Haider <87648696+Sidhugee@users.noreply.github.com>
2022-07-28 13:00:06 +0500

    Add files via upload

---
+++ ProductController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using OnlineShopping.Models;
+
+namespace OnlineShopping.Controllers
+{
+    public class ProductController : Controller
+    {
+        private Entities db = new Entities();
+
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View(db.Tbl_Product.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult Index(string search)
+        {
+            return View(db.Tbl_Product.Where(x=>x.ProductName.Contains(search) || search==null).ToList());
+        }
+
+      
+
+        // GET: Product/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tbl_Product tbl_Product = db.Tbl_Product.Find(id);
+            if (tbl_Product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbl_Product);
+        }
+    
+
+       
+        // GET: Product/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Product/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ProductId,ProductName,CategoryId,IsActive,IsDelete,CreatedDate,ModifiedDate,Description,ProductImage,IsFeatured,Quantity")] Tbl_Product tbl_Product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Tbl_Product.Add(tbl_Product);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tbl_Product);
+        }
+
+        // GET: Product/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tbl_Product tbl_Product = db.Tbl_Product.Find(id);
+            if (tbl_Product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbl_Product);
+        }
+
+        // POST: Product/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ProductId,ProductName,CategoryId,IsActive,IsDelete,CreatedDate,ModifiedDate,Description,ProductImage,IsFeatured,Quantity")] Tbl_Product tbl_Product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tbl_Product).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tbl_Product);
+        }
+
+        // GET: Product/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tbl_Product tbl_Product = db.Tbl_Product.Find(id);
+            if (tbl_Product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbl_Product);
+        }
+
+        // POST: Product/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Tbl_Product tbl_Product = db.Tbl_Product.Find(id);
+            db.Tbl_Product.Remove(tbl_Product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
